af36c434db0eab0ca56c23255f96633708f8f461
Mochammad Fawwaz <mailmcfawwaz@gmail.com>
2020-12-05 22:28:44 +0700

    progress pembuatan aplikasi cek ph

---
+++ 1.UAS/PhAir/app/src/main/java/id/ac/ub/phair/MainActivity.kt
@@ -0,0 +1,11 @@
+package id.ac.ub.phair
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
